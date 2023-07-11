09330eaa8b85c1ecdc4da2ede8ebc7d4db5f8deb
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-07-10 20:42:34 -0400

    Delete main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-module "neg" {
-    source="./networkendpointgroup"
-    region = ["northamerica-northeast1","us-central1"]
-    for_each={for region,function in local.cloud_functions_list: region => fucntion}
-    name="neg.${each.value.neg_name}"
-    project_id=var.project_id
-    function_name=each.value.name
-}
