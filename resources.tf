2de8cfa9dadd17e4555a4d782efb85312c3d773e
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-07-12 18:48:43 -0400

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,5 +1,5 @@
 resource "google_compute_region_network_endpoint_group" "function_neg" {
-  name                  = "neg-endpoint-1"
+  name                  = "function_neg"
   network_endpoint_type = "SERVERLESS"
   region                = "us-central1"
   cloud_function {
@@ -7,7 +7,7 @@ resource "google_compute_region_network_endpoint_group" "function_neg" {
   }
 }
 resource "google_compute_region_network_endpoint_group" "function_neg_2" {
-  name                  = "neg-endpoint-2"
+  name                  = "function_neg_2"
   network_endpoint_type = "SERVERLESS"
   region                = "northamerica-northeast1"
   cloud_function {
