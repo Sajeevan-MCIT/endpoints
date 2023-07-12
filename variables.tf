a2ca201542a545ee57fa5303e07841cf2da083d0
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-07-12 18:32:55 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,17 @@
-variable "project_id" {}
-variable "name"{}
-variable "function_name" {}
-variable "backend" {}
+variable "project_id" {
+  type = string
+}
+variable "name"{
+  type = string
+}
+variable "function_name" {
+  type = string
+}
+variable "backend" {
+  type = string
+}
 variable "region" {
 type = list(string) 
 default=["northamerica-northeast1","us-central1"]
 }
+
