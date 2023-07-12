29a11a9601ac2d448dbc82a10e0d3f73136541c3
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-07-12 19:25:16 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,9 +10,6 @@ variable "function_name" {
   type = list(string)
   default = ["test_functionsaj_name","test_functionsaj_name2"]
 }
-variable "backend" {
-  type = string
-}
 variable "region" {
 type = list(string) 
 default=["northamerica-northeast1","us-central1"]
