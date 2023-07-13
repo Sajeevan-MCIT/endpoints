e62f325e6cb8e8419c92d02db66d5387f2787730
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-07-12 20:19:48 -0400

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -8,3 +8,18 @@ resource "google_compute_region_network_endpoint_group" "neg" {
   }
   project = var.project_id
 }
+
+resource "google_compute_backend_service" "backend-arun" {
+  project = var.project
+
+  name        = "${var.name}-neg"
+  description = "neg Backend for ${var.name}"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+  enable_cdn  = false
+
+  backend {
+    group = google_compute_instance_group.neg.self_link
+  }
+}
